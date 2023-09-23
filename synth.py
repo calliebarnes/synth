0af69f125555fcef4383e4787a769291ac66754a
Callie Barnes <callie.barnes@g.austincc.edu>
2023-09-22 22:25:16 -0500

    assigned notes to keys on keyboard via pynput

--- synth.py
+++ synth.py
@@ -1,5 +1,21 @@
 import numpy as np
 import sounddevice as sd
+from pynput import keyboard
+
+NOTE_MAPPING = {
+    'a': 440.0,  # A4
+    'w': 466.16, # A#4/Bb4
+    's': 493.88, # B4
+    'd': 523.25, # C5
+    'r': 554.37, # C#5/Db5
+    'f': 587.33, # D5
+    't': 622.25, # D#5/Eb5
+    'g': 659.26, # E5
+    'h': 698.46, # F5
+    'u': 739.99, # F#5/Gb5
+    'j': 783.99, # G5
+    'i': 830.61  # G#5/Ab5
+}
 
 def generate_sine_wave(freq, duration, sample_rate=44100, amplitude=0.5):
     """Generate a sine wave of given frequency and duration."""
@@ -11,9 +27,18 @@ def play_sound(sound_array, sample_rate=44100):
     sd.play(sound_array, samplerate=sample_rate)
     sd.wait()
 
-if __name__ == "__main__":
-    frequency = float(input("Enter the frequency (in Hz): "))
-    duration = float(input("Enter the duration (in seconds): "))
+def on_key_release(key):
+    try:
+        key_char = key.char
+    except AttributeError:
+        # Skip special keys
+        return
 
-    sound = generate_sine_wave(frequency, duration)
-    play_sound(sound)
+    if key_char in NOTE_MAPPING:
+        sound = generate_sine_wave(NOTE_MAPPING[key_char], 0.5)
+        play_sound(sound)
+
+if __name__ == "__main__":
+    # Start listening to keyboard events
+    with keyboard.Listener(on_release=on_key_release) as listener:
+        listener.join()
